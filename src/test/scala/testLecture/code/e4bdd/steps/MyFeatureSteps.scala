e4dfab3c98ee8d8c2d8d5b383bed858ba02f6458
Roberto Casadei <robyonrails@gmail.com>
2022-05-10 15:56:28 +0200

    Fix var name + scope

--- src/test/scala/testLecture/code/e4bdd/steps/MyFeatureSteps.scala
+++ src/test/scala/testLecture/code/e4bdd/steps/MyFeatureSteps.scala
@@ -4,11 +4,11 @@ import io.cucumber.scala.{EN, ScalaDsl}
 import org.junit.jupiter.api.Assertions
 
 class MyFeatureSteps extends ScalaDsl with EN:
-  var (a, b, sum) = (0, 0, 0)
+  var (a, b, prod) = (0, 0, 0)
 
   Given("""^one operand (\d+)$"""){ a = (_:Int) }
   Given("""^another operand (\d+)$"""){ b = (_:Int) }
-  When("""^I multiply them together$"""){ sum = a*b  }
-  Then("""^I should obtain result (\d+)$"""){ (expectedSum:Int) =>
-    Assertions.assertEquals(expectedSum, sum)
+  When("""^I multiply them together$"""){ prod = a*b  }
+  Then("""^I should obtain result (\d+)$"""){ (expectedProduct: Int) =>
+    Assertions.assertEquals(expectedProduct, prod)
   }
